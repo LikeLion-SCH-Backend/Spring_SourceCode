1610fdf38c9d63210852b47aef0e7d3f39e9cf22
hong <kevin0928@naver.com>
2023-06-11 15:11:38 +0900

    [FIX] remove public

--- Spring-boot_secondLecuture/Blog/src/main/java/com/example/Blog/Repository/PostRepository.java
+++ Spring-boot_secondLecuture/Blog/src/main/java/com/example/Blog/Repository/PostRepository.java
@@ -4,5 +4,5 @@ import com.example.Blog.Entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    public Post findByContent(String content);
+    Post findByContent(String content);
 }
